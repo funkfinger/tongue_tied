9d6d973c7ba9e13cb5e6aeb06126e28693958f68
Jay Wiggins <jay@irk.com>
2013-06-23 22:32:28 -0700

    Ugh, posted something private...

--- tongue_tied.rb
+++ tongue_tied.rb
@@ -6,9 +6,7 @@ require 'twilio-ruby'
 class TongueTiedApp < Sinatra::Base
   register Sinatra::ConfigFile
   config_file './config.yml'
-  
-  @twilio_client = Twilio::REST::Client.new 'AC50c36451e9ccffe77249b8ca05936b1a', 'cc69499e93d89489afa13bc3fd9a31da'
-  
+    
   get '/' do
     "Tongue Tied App #{ENV['TWILIO_ACCOUNT_SID']}"
   end
