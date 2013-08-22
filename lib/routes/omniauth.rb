8a51a989f31a3c148f6a348c0a10249f8d1f4c2e
Jay Wiggins <jay@irk.com>
2013-08-22 12:09:41 -0700

    working on views

--- lib/routes/omniauth.rb
+++ lib/routes/omniauth.rb
@@ -4,12 +4,12 @@ class TongueTiedApp < Sinatra::Base
     halt 500 unless request.env['omniauth.auth']
     u = User.first_or_create_from_omniauth(request.env['omniauth.auth'])
     halt 500 unless u
-    flash[:notice] = "signed in - #{u.uid} | User.count = #{User.count}"
+    flash[:success] = "signed in - #{u.uid} | User.count = #{User.count}"
     redirect '/'
   end
 
   get '/auth/failure' do
-  	flash[:notice] = params[:message] # if using sinatra-flash or rack-flash
+  	flash[:error] = params[:message] # if using sinatra-flash or rack-flash
   	redirect '/'
   end
 
