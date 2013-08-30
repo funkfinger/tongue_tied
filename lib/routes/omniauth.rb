3e7e80ec4759304ba7cb5794d7712396f42c1717
Jay Wiggins <jay@irk.com>
2013-08-29 21:27:15 -0700

    added font-awesome

--- lib/routes/omniauth.rb
+++ lib/routes/omniauth.rb
@@ -1,11 +1,11 @@
-class TongueTiedApp < Sinatra::Base
+ class TongueTiedApp < Sinatra::Base
 
   get '/auth/:provider/callback' do
     halt 500 unless request.env['omniauth.auth']
     u = User.first_or_create_from_omniauth(request.env['omniauth.auth'])
     halt 500 unless u
-    flash[:success] = "signed in - #{u.uid} | User.count = #{User.count}"
-    session[:uid] = u.uid
+    session[:uid] = u.id
+    flash[:success] = "signed in - #{u.uid} | User.count = #{User.count} - #{session[:uid]}"
     redirect '/'
   end
 
