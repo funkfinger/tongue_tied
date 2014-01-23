845ceaf161d3aa9ae89fd85c2719ea59a379241a
Jay Wiggins <jay@irk.com>
2014-01-23 10:45:08 -0700

    removed auto response from Plivo (was a hack for the quiz)

--- lib/routes/plivo.rb
+++ lib/routes/plivo.rb
@@ -8,7 +8,7 @@ class TongueTiedApp < Sinatra::Base
   post '/api/plivo/sms' do
     halt 500, 'API error - failed to save' unless PlivoRequest.create_plivo_request(params)
     # removing the xml response because I think we should control the sending of messages elsewhere...
-    content_type 'text/xml'
-    PlivoRequest.response_xml("Cool, your quiz response was recieved... we'll let you know if you're a winner soon.", params['From'], params['To'])
+    # content_type 'text/xml'
+    # PlivoRequest.response_xml("Cool, your quiz response was recieved... we'll let you know if you're a winner soon.", params['From'], params['To'])
   end
 end
